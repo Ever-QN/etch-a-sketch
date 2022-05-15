9d3f744d04233196673189ae27c523e5b4d84fc2
Ever-QN <QuynhTNgo@live.com>
2022-05-15 00:17:12 -0600

    Centered button at top In the process of making a create new grid function

--- main.js
+++ main.js
@@ -14,11 +14,22 @@ function createSquareGrid (amount) {
     }
 }
 
+function createNewGrid () {
+    let userInput = window.prompt("Enter your new grid space (max is 100, making 100x100, min 1)");
+    
+    if (userInput <= Math.floor(100) && userInput >= Math.floor(1)) {
+
+    } else {
+        return "Error! Invalid input!";
+    }
+}
+
 function callNewGridBtn() {
     let createGridBtn = document.createElement("button");
     createGridBtn.classList.add("newGridBtn")
     createGridBtn.innerText = "Create new grid";
     document.body.insertBefore(createGridBtn, container);
+    createGridBtn.addEventListener("click", createNewGrid)
 }
 
 callNewGridBtn();
