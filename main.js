0ec1fedac29e4c9d505aa1e41c52bf5346b3b332
Ever-QN <QuynhTNgo@live.com>
2023-03-09 01:08:05 -0700

    Fix bug for user coloring page background

--- main.js
+++ main.js
@@ -11,28 +11,44 @@ function randomColor() {
 
 function createSquareGrid(amount) {
     for (let i = 0; i < amount; i++) {
-    let newDiv = document.createElement("div");
-    container.appendChild(newDiv);
-    newDiv.classList.add("squareClass");
-    newDiv.id = "squareid";
-    newDiv.style.minWidth = "25px";
-    newDiv.style.minHeight= "25px";
-    newDiv.style.backgroundColor = "#fff";
-    newDiv.addEventListener("mousedown", () => {
-        newDiv.style.backgroundColor = colorInput.value;
-        document.addEventListener("mousemove", colorSquare);
-    });
-    newDiv.addEventListener("mouseup", () => {
-        document.removeEventListener("mousemove", colorSquare);
-    });
+        let newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+        newDiv.classList.add("squareClass");
+        newDiv.id = "squareid";
+        newDiv.style.minWidth = "25px";
+        newDiv.style.minHeight = "25px";
+        newDiv.style.backgroundColor = "#fff";
+
+        newDiv.addEventListener("mousedown", () => {
+            newDiv.style.backgroundColor = colorInput.value;
+
+            // Add event listener to the entire document
+            document.addEventListener("mousemove", colorSquare);
+        });
+
+        newDiv.addEventListener("mouseup", () => {
+            document.removeEventListener("mousemove", colorSquare);
+        });
+}
+
+    // Function to check if mouse is within grid bounds
+function isMouseWithinGridBounds(e) {
+        const gridBounds = container.getBoundingClientRect();
+        return e.clientX >= gridBounds.left && e.clientX <= gridBounds.right && e.clientY >= gridBounds.top && e.clientY <= gridBounds.bottom;
+}
+
+    // Function to color the square when the mouse is moved within the grid bounds
+function colorSquare(e) {
+        if (!isMouseWithinGridBounds(e)) {
+            document.removeEventListener("mousemove", colorSquare);
+            return;
+        }
+        
+        let square = e.target;
+        square.style.backgroundColor = colorInput.value;
     }
 }
 
-function colorSquare(e) {
-    if (e.buttons !== 1) return; // exit function if left mouse button is not held down
-    let square = e.target;
-    square.style.backgroundColor = colorInput.value;
-}
 
 function removeElementsByClass(className){
     const elements = document.getElementsByClassName(className);
